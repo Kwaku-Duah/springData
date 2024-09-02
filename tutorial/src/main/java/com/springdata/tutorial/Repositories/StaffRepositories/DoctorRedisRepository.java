e22cc6c9bcb29bdc530b8d320b6a4d1f1c33cc28
Kwaku Duah <duah229@outlook.com>
2024-09-02 00:00:22 +0000

    Delete tutorial/src/main/java/com/springdata/tutorial/Repositories directory

--- tutorial/src/main/java/com/springdata/tutorial/Repositories/StaffRepositories/DoctorRedisRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springdata.tutorial.Repositories.StaffRepositories;
-
-import com.springdata.tutorial.Entities.StaffEntities.DoctorRedisEntity;
-import org.springframework.data.repository.CrudRepository;
-
-
-
-public interface DoctorRedisRepository extends CrudRepository<DoctorRedisEntity, String> {
-}
